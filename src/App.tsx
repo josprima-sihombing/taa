e0f8a1e2f680aa2c4ed2be51e204b328fca01333
josprima sihombing <jsihombing.id@gmail.com>
2025-05-05 22:50:24 +0700

    chore: setup mantine, absolute import and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    lazy: {
+      Component: async () => (await import("@/pages/login")).default,
+    },
+  },
+]);
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
+  );
 }
